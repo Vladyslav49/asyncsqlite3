dfcb04ef80004817e55e0922b1291c002849b56f
Vladyslav Timofeev <111080504+Vladyslav49@users.noreply.github.com>
2022-10-06 22:59:55 +0100

    Some changes to the row factory (version - 0.4.1).
    The __repr__ method of the Record - StringIO class (from io) is replaced by concatenation.

--- aiolite/factory.py
+++ aiolite/factory.py
@@ -1,5 +1,4 @@
 from sqlite3 import Row
-from io import StringIO
 
 
 class Record(Row):
@@ -10,14 +9,7 @@ class Record(Row):
             return default
 
     def __repr__(self):
-        with StringIO() as f:
-            f.write("<Record ")
-            count = 0
-            for key, value in dict(self).items():
-                count += 1
-                if len(dict(self)) == count:
-                    f.write(f"{key}={value}>")
-                else:
-                    f.write(f"{key}={value} ")
-            return f.getvalue()
-
+        result = f'<{type(self).__name__}'
+        for key, value in dict(self).items():
+            result += f' {key}={value}'
+        return f'{result}>'
